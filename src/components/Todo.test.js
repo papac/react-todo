27af63b080e574356737145efcee5662cfb19836
Franck DAKIA <dakiafranck@gmail.com>
2018-11-19 19:11:16 +0000

    remove test file

--- src/components/Todo.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Todo from './Todo';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Todo />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
